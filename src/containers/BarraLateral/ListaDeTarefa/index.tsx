7b5e744bf0caa7d1c8f76d4f4bfe8337a2af3d51
Gabriel Richard <richard.gabriel150@yahoo.com.br>
2023-11-20 21:23:46 -0300

    atualizando o filtro das tarefas

--- src/containers/BarraLateral/ListaDeTarefa/index.tsx
+++ src/containers/BarraLateral/ListaDeTarefa/index.tsx
@@ -6,17 +6,49 @@ import { Container } from './styles'
 import { RootReducer } from '../../../store'
 
 const ListaDeTarefas = () => {
-  const { tarefas } = useSelector((state: RootReducer) => state)
+  const { itens } = useSelector((state: RootReducer) => state.tarefas)
+  const { termo, criterio, valor } = useSelector(
+    (state: RootReducer) => state.filtro
+  )
+
+  const filtraTarefas = () => {
+    let tarefasFiltradas = itens
+    if (termo !== undefined) {
+      tarefasFiltradas = tarefasFiltradas.filter(
+        (item) => item.titulo.toLowerCase().search(termo.toLowerCase()) >= 0
+      )
+
+      if (criterio === 'prioridade') {
+        tarefasFiltradas = tarefasFiltradas.filter(
+          (item) => item.prioridade === valor
+        )
+      } else if (criterio === 'status') {
+        tarefasFiltradas = tarefasFiltradas.filter(
+          (item) => item.status === valor
+        )
+      }
+
+      return tarefasFiltradas
+    } else {
+      return itens
+    }
+  }
 
   return (
     <Container>
       <p>
-        2 tarefas marcadas como: &quot;categorias&ldquo; e &quot;termo&ldquo;
+        2 tarefas marcadas como: &quot;categorias&ldquo; e &quot;{termo}&ldquo;
       </p>
       <ul>
-        {tarefas.map((t) => (
+        <li>{termo}</li>
+        <li>{criterio}</li>
+        <li>{valor}</li>
+      </ul>
+      <ul>
+        {filtraTarefas().map((t) => (
           <li key={t.titulo}>
             <Tarefa
+              id={t.id}
               descricao={t.descricao}
               titulo={t.titulo}
               status={t.status}
