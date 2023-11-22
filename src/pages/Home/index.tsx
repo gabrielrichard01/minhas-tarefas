262508cffc07cbd90c2d8ab1e2820c78dd7399a8
Gabriel Richard <richard.gabriel150@yahoo.com.br>
2023-11-22 16:16:32 -0300

    criando uma pagina para cadastro

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import BarraLateral from '../../containers/BarraLateral'
+import ListaDeTarefas from '../../containers/BarraLateral/ListaDeTarefa'
+
+const Home = () => (
+  <>
+    <BarraLateral />
+    <ListaDeTarefas />
+  </>
+)
+
+export default Home
